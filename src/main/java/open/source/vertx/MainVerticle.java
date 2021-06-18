a6d4ee6cecd4375f361e795465350b18b460a778
Prakhar Makhija <matcdac@gmail.com>
2021-06-18 15:37:52 +0530

    [main] code refactoring for the http server java class

--- src/main/java/open/source/vertx/MainVerticle.java
+++ src/main/java/open/source/vertx/MainVerticle.java
@@ -1,26 +1,41 @@
 package open.source.vertx;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Handler;
 import io.vertx.core.Promise;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerRequest;
+import io.vertx.core.http.HttpServerResponse;
 
 public class MainVerticle extends AbstractVerticle {
 
-	@Override
-	public void start(Promise<Void> startPromise) throws Exception {
+	private static final Integer HTTP_SERVER_LISTENING_PORT = 8888;
 
-		vertx.createHttpServer()
-			.requestHandler(req -> {
-				req.response()
-					.putHeader("content-type", "text/plain")
-					.end("Hello from Vert.x!");
-			})
-			.listen(8888, http -> {
-				if (http.succeeded()) {
-					startPromise.complete();
-					System.out.println("HTTP server started on port 8888");
-				} else {
-					startPromise.fail(http.cause());
-				}
-			});
+	private static Handler<HttpServerRequest> HTTP_SERVER_REQUEST_HANDLER = (httpServerRequest) -> {
+
+		HttpServerResponse httpServerResponse = httpServerRequest.response()
+				.putHeader("content-type", "text/plain");
+		httpServerResponse.end("Hello from Vert.x!");
+	};
+
+	private static void rejectOrResolvePromise(Promise<Void> promiseRequest, AsyncResult<HttpServer> taskResponse) {
+
+		if (taskResponse.succeeded()) {
+			promiseRequest.complete();
+			System.out.println("HTTP server started on port 8888");
+		} else {
+			promiseRequest.fail(taskResponse.cause());
+		}
 	}
+
+	@Override
+	public void start(Promise<Void> promise) throws Exception {
+
+		HttpServer httpServer = vertx.createHttpServer();
+		httpServer = httpServer.requestHandler(HTTP_SERVER_REQUEST_HANDLER);
+		httpServer = httpServer.listen(HTTP_SERVER_LISTENING_PORT,
+				httpServerAsyncResult -> rejectOrResolvePromise(promise, httpServerAsyncResult));
+	}
+
 }
